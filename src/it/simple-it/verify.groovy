08d86a261d8dafa86255e83bca4cb64b48c83093
sl <slazarus@ipswitch.com>
2015-07-27 14:16:12 -0400

    switched text to getText() in verify.groovy

--- src/it/simple-it/verify.groovy
+++ src/it/simple-it/verify.groovy
@@ -2,7 +2,7 @@ import groovy.json.JsonSlurper
 File dir = classesDirectory
 def reportFile = new File( dir, "/spel-maven-plugin/report.txt" );
 assert reportFile.isFile()
-def contents = new JsonSlurper().parseText(reportFile.text)
+def contents = new JsonSlurper().parseText(reportFile.getText())
 assert contents instanceof Map
 
 assert contents.getProperty("checked") == properties.getProperty("spel-annotations-amount")
